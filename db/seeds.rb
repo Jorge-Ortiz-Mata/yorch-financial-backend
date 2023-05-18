3d5a42c5a9aa5830fcb40d65c916968330aff92c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-05-18 14:36:23 -0500

    Adding objects configuration for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.create(name: 'First post')
+
+Post.create(name: 'Second post')
+
+Post.create(name: 'Third post')
