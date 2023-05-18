cee9cbabb0ccb39e250a5b746b74060cf1f69e6a
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-05-18 17:21:22 -0500

    Adding the folder v1 into the api folder.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :posts
+    namespace :v1 do
+      resources :posts
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
